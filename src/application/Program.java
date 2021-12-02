a67a05ba6ff7b53e9d99e8757d5dc5aafa9419c6
Rafael Camargo <rafael.camargo384@gmail.com>
2021-12-02 15:12:42 -0300

    Reducing findById method and code improvement

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,18 +12,11 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		
-		Department obj = new Department(1, "Books");
-		Seller obj1 = new Seller(21, "Bob Brown", "bob@gmail.com", new Date(), 3000.0, obj);
-
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-
-		System.out.println("Insert ID: ");
-		int id = sc.nextInt();
 		
-		Seller seller = sellerDao.findById(id);
-
+		
+		System.out.println("====TESTE1: Seller findById====");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 
 	}
